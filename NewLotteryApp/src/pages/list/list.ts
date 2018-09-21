a584cc4005d312b9462a8825454c0d206ef80d21
avishai <avishai1232@gmail.com>
2018-09-21 19:38:57 +0300

    [edit ]list changed in order to show the last week winners

--- NewLotteryApp/src/pages/list/list.ts
+++ NewLotteryApp/src/pages/list/list.ts
@@ -21,9 +21,9 @@ export class ListPage {
     this.items = [];
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: 'prize: ' + (Math.random() * Math.floor(10000)),
+        note: 'winner number #' + i,
+        icon: 'person'
       });
     }
   }
