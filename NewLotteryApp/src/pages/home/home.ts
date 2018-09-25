0aeead0dfd3a63cd0dec879cc39a319e5f9e13c8
avishai <avishai1232@gmail.com>
2018-09-25 19:51:24 +0300

    [added] share button now work on the main screen

--- NewLotteryApp/src/pages/home/home.ts
+++ NewLotteryApp/src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  message = 'message';
+  subject = 'subject message';
+  file = null;
+  link = 'www.google.com';
 
-  constructor(public navCtrl: NavController) {
+  constructor(private socialSharing: SocialSharing) { }
 
+  sharing() {
+    this.socialSharing.share(this.message, this.subject, this.file, this.link).then(() => {
+    }).catch((e) => {
+      alert(e);
+    })
   }
 
 }
