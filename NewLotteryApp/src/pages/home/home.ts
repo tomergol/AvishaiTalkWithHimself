3213b6b3e7c6ba2a9192876c36696ee6a5879d5b
avishai <avishai1232@gmail.com>
2018-10-04 22:12:20 +0300

    [edited] temp style

--- NewLotteryApp/src/pages/home/home.ts
+++ NewLotteryApp/src/pages/home/home.ts
@@ -11,8 +11,26 @@ export class HomePage {
   subject = 'subject message';
   file = null;
   link = 'www.google.com';
-
-  constructor(private socialSharing: SocialSharing) { }
+  data;
+  constructor(private socialSharing: SocialSharing) {
+    this.data = {
+      labels: ['people','tickets'],
+      datasets: [
+          {
+              data: [50, 124],
+              backgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  // "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  // "#FFCE56"
+              ]
+          }]    
+      };
+   }
 
   sharing() {
     this.socialSharing.share(this.message, this.subject, this.file, this.link).then(() => {
